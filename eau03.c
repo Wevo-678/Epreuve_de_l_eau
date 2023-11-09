9515c809a63f9710c19ec39bc0d1e09e668693b8
Maxime Benet <mabenet@c2r3p9.42perpignan.fr>
2023-11-09 17:25:03 +0100

    push 3

---
+++ eau03.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+int fibonnaci(int n)
+{
+	int result;
+	int	a;
+	int	b;
+	int	i;
+
+	result = 0;
+	a = 0;
+	b = 1;
+	i = 2;
+	if (n < 0)
+		result = -1;
+	else if (n == 0)
+		result = 0;
+	else if (n == 1)
+		result = 1;
+	else
+	{
+		while (i <= n)
+		{
+			result = a + b;
+			a = b;
+			b = result;
+			i++;
+		}
+	}
+	return (result);
+}
+
+int secure(char *s)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+	{
+		if (('0' >= s[i] && s[i] >= '9'))
+			return (0); 
+		i++;
+	}
+	return (1);
+}
+
+int main(int ac, char **av) 
+{
+	if (ac == 2)
+	{
+		if (secure(av[1]) == 1)
+			printf("%d\n",fibonnaci(atoi(av[1])));
+		else
+			printf("Erreur d'argument !");	
+	}
+	else
+		printf("Erreur d'argument !");
+}
